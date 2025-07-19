d8e576ab9b35cb8073f16b59a8354320d3680271
Sky <skywangjian1234@qq.com>
2025-07-19 17:22:59 +0800

    refactor project structure and add tests project

---
+++ OrderApi.Tests/Shared/TestHelpers.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using OrderApi.Data;
+using OrderApi.Data.DTOs;
+using OrderApi.Data.DTOs.Input;
+using OrderApi.Data.Profiles;
+using OrderApi.Data.Repositories.Implementations;
+using OrderApi.Data.Repositories.Interfaces;
+using OrderApi.Services.Implementations;
+using OrderApi.Services.Interfaces;
+using FluentAssertions;
+using FluentAssertions.Primitives;
+
+namespace OrderApi.Tests.Shared
+{
+    public static class TestHelpers
+    {
+        public static IMapper CreateTestMapper()
+        {
+            var services = new ServiceCollection();
+            services.AddAutoMapper(cfg => cfg.AddProfile<OrderProfile>());
+
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider.GetRequiredService<IMapper>();
+        }
+
+        public static ILogger<T> CreateTestLogger<T>()
+        {
+            return Mock.Of<ILogger<T>>();
+        }
+
+        public static AppDbContext CreateInMemoryDbContext(string dbName = "TestDb")
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            var context = new AppDbContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+
+        public static IOrderRepository CreateOrderRepository(AppDbContext context = null)
+        {
+            context ??= CreateInMemoryDbContext();
+            var logger = CreateTestLogger<OrderRepository>();
+            return new OrderRepository(context, logger);
+        }
+
+        public static IOrderService CreateOrderService(
+            IOrderRepository repository = null,
+            IMapper mapper = null)
+        {
+            repository ??= Mock.Of<IOrderRepository>();
+            mapper ??= CreateTestMapper();
+            var logger = CreateTestLogger<OrderService>();
+            return new OrderService(repository, mapper, logger);
+        }
+
+        public static CreateOrderDto CreateValidOrderDto()
+        {
+            return new CreateOrderDto
+            {
+                CustomerName = "Test Customer",
+                Items = new List<OrderItemDto>
+            {
+                new OrderItemDto
+                {
+                    ProductId = Guid.NewGuid(),
+                    Quantity = 5
+                }
+            }
+            };
+        }
+
+        // NUnit 特定的断言扩展
+        public static void ShouldBeEquivalentWithDetails(this object actual, object expected, string because = "")
+        {
+            actual.Should().BeEquivalentTo(expected, options => options
+            .WithStrictOrdering(), 
+            because);
+        }
+    }
+}
